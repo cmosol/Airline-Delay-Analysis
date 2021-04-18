78630b1b8947a40b392ab8c57dad7e52c2568cf3
cmosol <46791335+cmosol@users.noreply.github.com>
2021-04-17 20:48:44 -0500

    add cancelledflights.py

---
+++ src/Spark/CancelledFlights/CancelledFlights.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import asc
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import sum
+
+#create a spark session
+spark = SparkSession.builder.appName('CancelledFlights').getOrCreate()
+
+#create data frame from flight data
+df = spark.read.option("header", True).csv('file input path')
+#cast cancelled column to integer
+df = df.withColumn("CANCELLED", df["CANCELLED"].cast(IntegerType()))
+#group by airline carrier, sum all cancellations per carrier, order cancellation total in asc order
+df.groupBy("OP_CARRIER").sum("CANCELLED").orderBy(asc("sum(CANCELLED)")).show()
